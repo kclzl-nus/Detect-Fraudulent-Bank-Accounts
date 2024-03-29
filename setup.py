9021be93b2e1da566e9f2a850afffb7e8edc472f
Kyler Cheong <kylercheong123@gmail.com>
2024-03-29 16:22:58 +0800

    Added new files following template from cookie-cutter.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Detecting fraudent t bank accounts and gain insights into characteristics of these fraudent accounts and generate actionable insights to detect and prevent these kind of fraud.',
+    author='Kyler Cheong',
+    license='MIT',
+)
